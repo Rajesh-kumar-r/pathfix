5a62cc2ab0cd8cba30d54f1d43b645256062f082
Rajesh kumar R <47350245+Rajesh-kumar-r@users.noreply.github.com>
2021-03-02 09:47:25 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ import React from "react";
 import { Button } from "antd";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
-
 class LoginForm extends React.Component {
   componentDidMount() {
     window.addEventListener("$pinc.ui.auth.loggedin", () => {
       // To show user details on success
       console.log("ready", window.$pinc.auth.profile);
     });
-    window.addEventListener("$pinc.oauth.notConsented", () => {
+    window.addEventListener("$pinc.oauth.notconsented", () => {
       // To show error message in login failed
       console.log("error occurred");
     });
+    //load the helper script
+    const script = document.createElement("script");
+    script.id = "pinc.helper";
+    script.src = "https://labs.pathfix.com/helper.js";
+    script.setAttribute("modules", "pinc.auth.min");
+    script.setAttribute(
+      "data-client-id",
+      "3685AD5D-C8A0-4734-837F-822341D2D03C"
+    );
+    script.setAttribute(
+      "data-client-providers",
+      "msazuread,discord,fb,github,google,linkedin,ms,spotify,twitter"
+    );
+    document.body.appendChild(script);
   }
   loggedIn = () => {
     window.location.replace(
