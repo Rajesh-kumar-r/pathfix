a557013d8606962af0d4c3b92cef571a3b5b501e
Rajesh kumar R <47350245+Rajesh-kumar-r@users.noreply.github.com>
2021-03-03 14:06:53 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Space } from "antd";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: "once login profile will be here"
+    };
+  }
   componentDidMount() {
     window.addEventListener("$pinc.ui.auth.loggedin", () => {
       // To show user details on success
+      this.setState({ profile: JSON.stringify(window.$pinc.auth.profile) });
       console.log("ready", window.$pinc.auth.profile);
     });
     window.addEventListener("$pinc.oauth.notconsented", () => {
@@ -18,13 +25,23 @@ class LoginForm extends React.Component {
       `https://labs.pathfix.com/integrate/command?provider=fbidentity&public_key=3685AD5D-C8A0-4734-837F-822341D2D03C&consented_redirect=${window.location.href}&consented_action=redirect`
     );
   };
+  loggedInGoogle = () => {
+    window.location.replace(
+      `https://labs.pathfix.com/integrate/command?provider=googleidentity&public_key=B318DA15-B885-4622-9380-42F19B2C7E68&consented_redirect=${window.location.href}&consented_action=redirect`
+    );
+  };
   render() {
     return (
-      <Button block={true} onClick={this.loggedIn} type="primary">
-        Login
-      </Button>
+      <Space>
+        <Button onClick={this.loggedIn} type="primary">
+          Login
+        </Button>
+        <Button onClick={this.loggedInGoogle} type="primary">
+          google
+        </Button>
+        {this.state.profile}
+      </Space>
     );
   }
 }
 ReactDOM.render(<LoginForm />, document.getElementById("container"));
-
